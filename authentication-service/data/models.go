eb9eceece4afd051b7f9d8e57e163ca54b11526d
huyenph <huyen.ph4m@gmail.com>
2025-09-13 16:37:26 +0700

    fix: models

--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -14,23 +14,6 @@ const dbTimeout = time.Second * 3
 
 var db *sql.DB
 
-// New is the function used to create an instance of the data package. It returns the type
-// Model, which embeds all the types we want to be available to our application.
-func New(dbPool *sql.DB) Models {
-	db = dbPool
-
-	return Models{
-		User: User{},
-	}
-}
-
-// Models is the type for this package. Note that any model that is included as a member
-// in this type is available to us throughout the application, anywhere that the
-// app variable is used, provided that the model is also added in the New function.
-type Models struct {
-	User User
-}
-
 // User is the structure which holds one user from the database.
 type User struct {
 	ID        int       `json:"id"`
@@ -41,6 +24,23 @@ type User struct {
 	Active    int       `json:"active"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// Models is the type for this package. Note that any model that is included as a member
+// in this type is available to us throughout the application, anywhere that the
+// app variable is used, provided that the model is also added in the New function.
+type Models struct {
+	User User
+}
+
+// New is the function used to create an instance of the data package. It returns the type
+// Model, which embeds all the types we want to be available to our application.
+func New(dbPool *sql.DB) Models {
+	db = dbPool
+
+	return Models{
+		User: User{},
+	}
 }
 
 // GetAll returns a slice of all users, sorted by last name
